c9ae8bde98d68456d72bbd9eef24049268379fa6
Khem Raj Pradhan <prdhn.khem@gmail.com>
2025-09-21 11:21:07 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,48 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggleButton = document.getElementById('dark-mode-toggle');
     const currentTheme = localStorage.getItem('theme');
 
-    // Apply the saved theme on initial load
     if (currentTheme === 'dark') {
         document.body.classList.add('dark-mode');
     }
 
     themeToggleButton.addEventListener('click', () => {
-        // Toggle the .dark-mode class on the body
         document.body.classList.toggle('dark-mode');
-
-        // Check if dark mode is currently enabled and save preference
-        let theme = 'light';
-        if (document.body.classList.contains('dark-mode')) {
-            theme = 'dark';
-        }
+        let theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
         localStorage.setItem('theme', theme);
+    });
+
+
+    // Sub-Menu Toggle for Mobile
+    const submenuItems = document.querySelectorAll('.has-submenu');
+
+    submenuItems.forEach((item) => {
+        const link = item.querySelector('a');
+        link.addEventListener('click', (event) => {
+            // Check if the window width is for mobile view
+            if (window.innerWidth < 992) {
+                // Prevent link from navigating on first click
+                event.preventDefault();
+                
+                // Toggle the 'submenu-open' class on the parent li
+                item.classList.toggle('submenu-open');
+                
+                // Close other open submenus
+                submenuItems.forEach((otherItem) => {
+                    if (otherItem !== item) {
+                        otherItem.classList.remove('submenu-open');
+                    }
+                });
+            }
+        });
+    });
+
+    // Close submenus if clicking outside
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.has-submenu')) {
+            submenuItems.forEach((item) => {
+                item.classList.remove('submenu-open');
+            });
+        }
     });
 
 });
